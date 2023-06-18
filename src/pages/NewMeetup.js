4ca237f1bc3a12a69544378825842ec1ab33d0ed
Bharavi Lakhote <bharavi15@gmail.com>
2023-06-18 11:37:21 +0530

    Meetups HTTP requests handled

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,24 @@
-function NewMeetupPage(props){
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage(props) {
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-727a0-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(()=>history.replace('/'));
+  }
   return (
-  <div>
-  New Meetup Page
-  </div>
-);
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
